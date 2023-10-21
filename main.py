b688a9abbfc2084db87497bc36ce6f67d9a51c5f
Nobody <mishasikolenko@mail.ru>
2023-10-21 13:34:56 +0300

    Init commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def send_number_to_server(number):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(('127.0.0.1', 2000))
 
-        # Отправляем число на сервер
         client.send(str(number).encode('utf-8'))
 
         response = b''
