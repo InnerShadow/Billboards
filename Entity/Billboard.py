3561540c8a9340b4adafddfc55164d4f26d89b05
Nobody <mishasikolenko@mail.ru>
2023-10-25 19:56:58 +0300

    Fix Billboards:

--- Entity/Billboard.py
+++ Entity/Billboard.py
@@ -1,9 +1,10 @@
 
 class BiilBoard:
-    def __init__(self, billboard_id, x, y):
+    def __init__(self, billboard_id, x, y, billboards_groop_name):
         self.billboard_id = billboard_id
         self.x = x
         self.y = y
+        self.billboards_groop_name = billboards_groop_name
         
 
 
