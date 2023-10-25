57c35faa319dcca8694cfd080c57c5012a47fb2f
Nobody <mishasikolenko@mail.ru>
2023-10-25 16:08:02 +0300

    Make auto resize events

--- Windows/MainWindow.py
+++ Windows/MainWindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 from PyQt5.QtGui import QPixmap
 import re
 
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
         pattern = r'X: (\d+), Y: (\d+)'
         matches = re.findall(pattern, response_text)
 
-        coordinates = [(int(x), int(y)) for x, y in matches]
+        coordinates = [(int(x) / 1000, int(y) / 1000) for x, y in matches]
         return coordinates
 
 
@@ -39,22 +39,41 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.view.setScene(self.scene)
 
-        background_image = QPixmap('Data/Jodino.png')
-        background_item = QGraphicsPixmapItem(background_image)
-        background_item.setZValue(0)
-        self.scene.addItem(background_item)
+        self.background_image = QPixmap('Data/Jodino.png')
+        self.background_item = QGraphicsPixmapItem(self.background_image)
+        self.background_item.setZValue(0)
+        self.scene.addItem(self.background_item)
 
+        self.resizeEvent = self.handleResize
+
+        self.updateGraphicsItems()
+
+        self.show()
+
+
+    @pyqtSlot()
+    def handleResize(self, event):
+        self.updateGraphicsItems()
+
+
+    def updateGraphicsItems(self):
         client = Client('127.0.0.1', 2000)
+        bg_width = self.view.viewport().width()
+        bg_height = self.view.viewport().height()
+        self.background_item.setPixmap(self.background_image.scaled(bg_width, bg_height, Qt.KeepAspectRatio))
+
+        for item in self.scene.items():
+            if isinstance(item, QGraphicsRectItem):
+                self.scene.removeItem(item)
+
         billboards_positions = self.parse_billboards(client.Get_Billboards('GET_BILLBOARDS'))
+        for x, y in billboards_positions:
+            x = int(x * bg_width)
+            y = int(y * bg_height)
 
-        rectangles = []
-        for i in billboards_positions:
-            rectangles.append(i + (self.billboard_w, self.billboard_h))
-
-        for rect in rectangles:
-            x, y, w, h = rect
+            rect = QRect(x, y, self.billboard_w, self.billboard_h)
+            scaled_rect = rect.intersected(self.background_item.boundingRect().toRect())
+            x, y, w, h = scaled_rect.x(), scaled_rect.y(), scaled_rect.width(), scaled_rect.height()
             item = QGraphicsRectItem(x, y, w, h)
             item.setBrush(Qt.black)
             self.scene.addItem(item)
-
-        self.show()
