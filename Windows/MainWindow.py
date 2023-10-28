3d0dc76b225c0dae47dce6d24288c11e08f210fd
Nobody <mishasikolenko@mail.ru>
2023-10-28 14:49:59 +0300

    Change class methods

--- Windows/MainWindow.py
+++ Windows/MainWindow.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtGui import QPixmap
 import re
 
@@ -37,7 +39,7 @@ class MainWindow(QMainWindow):
             if_add = True
 
             for i in self.billboards_groops:
-                if i.groop_name == groop_name and i.schedules == schedules:
+                if i.groop_name == groop_name and i.schedules_name == schedules:
                     if_add = False
 
             if if_add:
@@ -64,10 +66,9 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.view.setScene(self.scene)
 
-        self.background_image = QPixmap('Data/Jodino.png')
-        self.background_item = QGraphicsPixmapItem(self.background_image)
-        self.background_item.setZValue(0)
-        self.scene.addItem(self.background_item)
+        self.init_beckground()
+
+        self.init_close_button()
 
         self.resizeEvent = self.handleResize
 
@@ -84,19 +85,48 @@ class MainWindow(QMainWindow):
     def updateGraphicsItems(self):
         bg_width = self.view.viewport().width()
         bg_height = self.view.viewport().height()
-        self.background_item.setPixmap(self.background_image.scaled(bg_width, bg_height, Qt.KeepAspectRatio))
+
+        self.updateCloseButton(bg_width)
+
+        self.updateBeackground(bg_width, bg_height)
 
         for item in self.scene.items():
             if isinstance(item, QGraphicsRectItem):
                 self.scene.removeItem(item)
 
-        # for x, y in self.billboards_positions:
-        #     x = int(x * bg_width)
-        #     y = int(y * bg_height)
+        self.updateBillboards(min([bg_width, bg_height]))
 
-        #     rect = QRect(x, y, self.billboard_w, self.billboard_h)
-        #     scaled_rect = rect.intersected(self.background_item.boundingRect().toRect())
-        #     x, y, w, h = scaled_rect.x(), scaled_rect.y(), scaled_rect.width(), scaled_rect.height()
-        #     item = QGraphicsRectItem(x, y, w, h)
-        #     item.setBrush(Qt.black)
-        #     self.scene.addItem(item)
+
+    def updateBeackground(self, bg_width : int, bg_height : int):
+        self.background_item.setPixmap(self.background_image.scaled(bg_width, bg_height, Qt.KeepAspectRatio))
+
+
+    def updateCloseButton(self, bg_width : int):
+        self.close_button.setGeometry(bg_width - 80, 10, self.close_button.width(), self.close_button.height())
+
+
+    def updateBillboards(self, min_size : int):
+        for groop in self.billboards_groops:
+            for billboard in groop.BillBoards:
+                x = int(billboard.x * min_size)
+                y = int(billboard.y * min_size)
+
+                rect = QRect(x, y, self.billboard_w, self.billboard_h)
+                scaled_rect = rect.intersected(self.background_item.boundingRect().toRect())
+                x, y, w, h = scaled_rect.x(), scaled_rect.y(), scaled_rect.width(), scaled_rect.height()
+                item = QGraphicsRectItem(x, y, w, h)
+                item.setBrush(Qt.black)
+                self.scene.addItem(item)
+
+
+    def init_beckground(self):
+        self.background_image = QPixmap('Data/Jodino.png')
+        self.background_item = QGraphicsPixmapItem(self.background_image)
+        self.background_item.setZValue(0)
+        self.scene.addItem(self.background_item)
+
+
+    def init_close_button(self):
+        self.close_button = QPushButton('Exit', self)
+        self.close_button.clicked.connect(self.close)
+
